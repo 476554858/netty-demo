c476414810de48fbe3fbd3415ef01c2400836525
476554858@qq.com <zjx93021947220>
2020-06-07 20:52:17 +0800

    增加同步请求的方式

--- src/main/java/com/zjx/bilinio/basic/NettyClient.java
+++ src/main/java/com/zjx/bilinio/basic/NettyClient.java
@@ -3,14 +3,19 @@ package com.zjx.bilinio.basic;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPromise;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
+import java.util.concurrent.TimeUnit;
+
 //网络客户端
 public class NettyClient {
     public static void main(String[] args) throws Exception{
+
+        final NettyClientHandler nettyClientHandler = new NettyClientHandler();
         //1.创建一个线程组
         EventLoopGroup group=new NioEventLoopGroup();
         //2.创建客户端的启动助手，完成相关配置
@@ -20,7 +25,7 @@ public class NettyClient {
         .handler(new ChannelInitializer<SocketChannel>() { //5.创建一个初始胡对象
             @Override//6.往pipeline通道中添加自定义的handler
             protected void initChannel(SocketChannel socketChannel) throws Exception {
-                socketChannel.pipeline().addLast(new NettyClientHandler());
+                socketChannel.pipeline().addLast(nettyClientHandler);
             }
         });
         System.out.println(".......Client is ready");
@@ -28,7 +33,12 @@ public class NettyClient {
         //7.启动客户端去连接服务器端 connect方法是异步的 sync方法是同步非阻塞的
         ChannelFuture cf = b.connect("127.0.0.1",9999).sync();
 
+        ChannelPromise promise = nettyClientHandler.sendMessage("xxx");
+        promise.await(1, TimeUnit.SECONDS);
+        String result = (String)nettyClientHandler.getResponse();
+        System.out.println("reuslt:"+result);
         //8.关闭连接（异步非阻塞）
-        cf.channel().closeFuture().sync();
+//        cf.channel().closeFuture().sync();
+        cf.channel().close();
     }
 }
--- src/main/java/com/zjx/bilinio/basic/NettyClientHandler.java
+++ src/main/java/com/zjx/bilinio/basic/NettyClientHandler.java
@@ -4,18 +4,47 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
 import io.netty.util.CharsetUtil;
+
+import java.util.concurrent.TimeUnit;
 
 //客户端业务处理类
 public class NettyClientHandler extends ChannelInboundHandlerAdapter{
+    private ChannelHandlerContext ctx;
+
+    private Object response;
+
+    private ChannelPromise promise;
+
     //通道就绪事件
     public void channelActive(ChannelHandlerContext ctx){
+        this.ctx = ctx;
         System.out.println("Client:"+ctx);
         ctx.writeAndFlush(Unpooled.copiedBuffer("老板，还钱吧", CharsetUtil.UTF_8));
     }
     //读取数据事件
     public void channelRead(ChannelHandlerContext ctx,Object msg){
         ByteBuf buf=(ByteBuf)msg;
-        System.out.println("服务器端发来的消息"+buf.toString(CharsetUtil.UTF_8));
+//        System.out.println("服务器端发来的消息"+buf.toString(CharsetUtil.UTF_8));
+        response = buf.toString(CharsetUtil.UTF_8);
+        promise.setSuccess();
+    }
+
+    public synchronized ChannelPromise sendMessage(Object message) {
+        while (ctx == null) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(1);
+                //logger.error("等待ChannelHandlerContext实例化");
+            } catch (InterruptedException e) {
+            }
+        }
+        promise = ctx.newPromise();
+        ctx.writeAndFlush(message);
+        return promise;
+    }
+
+    public Object getResponse(){
+        return response;
     }
 }
